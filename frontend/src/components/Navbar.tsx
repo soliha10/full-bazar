5d58398b76154b56914d9e2ba9030c1ebb3545b6
soliha10 <toxtamurodovagulshodaa@gmail.com>
2026-02-04 17:57:58 +0500

    fix bg

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -19,7 +19,7 @@ export function Navbar({ onSearchChange }: NavbarProps) {
   ];
 
   return (
-    <nav className="glass sticky top-0 z-50 border-b border-border/50">
+    <nav className=" bg-white sticky top-0 z-50 border-b border-border/50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
